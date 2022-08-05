baf0b2943f28e1270b39ab6a73c761820cc79331
aktech27 <ahamedkbr85@gmail.com>
2022-08-05 22:16:56 +0530

    Fix UI - Added CSS

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,34 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/Providers/AuthContext";
+import styles from "./Home.module.css";
 
 const Home = () => {
   const { user } = useContext(AuthContext);
   return (
-    <div>
-      <h1>Home</h1>
-      <h2>Current Login Details:</h2>
-      <p>(Login details from AuthContext - user state)</p>
-      {user ? (
-        <h3>Logged in as : {user.email}</h3>
-      ) : (
-        <h3>User is Logged out</h3>
-      )}
+    <div className={`flex-cc ${styles.container}`}>
+      <h1>Home Component</h1>
+      <div className={styles.card}>
+        <div>
+          <h2>Login Details:</h2>
+          <p>Login details from AuthContext - user state</p>
+        </div>
+        {user ? (
+          <div>
+            <h3>User is logged in.</h3>
+            <span>
+              <b>Name : </b>
+              {user.name}
+            </span>
+            <br />
+            <span>
+              <b>Email : </b>
+              {user.email}
+            </span>
+          </div>
+        ) : (
+          <h3>User is Logged out</h3>
+        )}
+      </div>
     </div>
   );
 };
