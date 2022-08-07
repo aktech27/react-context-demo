e4bcf0c27899418b8cc06a02a2b18f9bea31fde4
aktech27 <ahamedkbr85@gmail.com>
2022-08-07 20:05:56 +0530

    Build

--- src/components/NotFound/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const NotFound = () => {
-  const location = useLocation();
-  return <div>OOPS ! The given URL : {location.pathname} is not found</div>;
-};
-
-export default NotFound;
